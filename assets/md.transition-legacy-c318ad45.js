81294c90ca83872fea14154613acfa6e85ca0513
OkayKenji <okaykenji@icloud.com>
2023-11-21 12:28:29 -0500

    Updates

--- assets/md.transition-legacy-c318ad45.js
+++ assets/md.transition-legacy-c318ad45.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-dc546904.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-61d8e985.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
