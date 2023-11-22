656217bab7759af00ce11cbf94e9f559cdd33dec
OkayKenji <okaykenji@icloud.com>
2023-11-21 22:11:47 -0500

    Updates

--- assets/ios.transition-legacy-c82d53ae.js
+++ assets/ios.transition-legacy-c82d53ae.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-595d1ae3.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-8435e346.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
