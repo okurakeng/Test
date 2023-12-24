29c6d256344c8f6217339c0744df4b586a9a8493
Kenji <70079754+OkayKenji@users.noreply.github.com>
2023-12-23 20:12:02 -0500

    Updates

--- assets/md.transition-legacy-cb64d4ce.js
+++ assets/md.transition-legacy-cb64d4ce.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-190ddd36.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-ae0d1fb9.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
