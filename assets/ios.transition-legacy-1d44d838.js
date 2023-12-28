3c8c711dda157c46ec7f7eb995a51b893b277fdc
Kenji <70079754+OkayKenji@users.noreply.github.com>
2023-12-28 03:36:24 -0500

    Updates

--- assets/ios.transition-legacy-1d44d838.js
+++ assets/ios.transition-legacy-1d44d838.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-619a1e85.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-2b1088b0.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
--- assets/md.transition-legacy-3ef03d53.js
+++ assets/md.transition-legacy-3ef03d53.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-619a1e85.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
+System.register(["./index-legacy-2b1088b0.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.q,i=e.o}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
