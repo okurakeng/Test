e63b0fbf2f4037579aad6109faab7a4e2287115e
Kenji <70079754+OkayKenji@users.noreply.github.com>
2024-01-25 20:43:30 -0500

    Updates

--- assets/ios.transition-legacy-a4c95fc2.js
+++ assets/ios.transition-legacy-a4c95fc2.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-aef5e81c.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
+System.register(["./index-legacy-a0ccfaee.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.o,n=t.q}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
