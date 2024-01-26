d7abadea76aeb07f142ca6a3418568bd56a36698
Kenji <70079754+OkayKenji@users.noreply.github.com>
2024-01-25 21:07:43 -0500

    Updates

--- assets/swipe-back-legacy-cffca58f.js
+++ assets/swipe-back-legacy-cffca58f.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-a0ccfaee.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-78f0bc39.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
        * (C) Ionic http://ionicframework.com - MIT License
        */
